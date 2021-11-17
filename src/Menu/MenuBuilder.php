07b98780f220ac5dd424120c2de78686c4c014b0
Theodor Brown <theodorbrown@macbook-air-de-theodor.home>
2021-11-17 22:18:06 +0100

    login et templating

--- src/Menu/MenuBuilder.php
+++ src/Menu/MenuBuilder.php
@@ -4,6 +4,7 @@ namespace App\Menu;
 
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
+use Symfony\Component\Security\Core\Security;
 
 class MenuBuilder
 {
@@ -12,7 +13,7 @@ class MenuBuilder
     /**
      * Add any other dependency you need...
      */
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, private Security $security)
     {
         $this->factory = $factory;
     }
@@ -24,7 +25,19 @@ class MenuBuilder
         $menu->addChild('Accueil', ['route' => 'home']);
         $menu->addChild('Les Pizzas', ['route' => 'pizza_index']);
         $menu->addChild('Les Catégories', ['route' => 'category_index']);
+        $menu->addChild('Les Catégories', ['route' => 'category_index']);
         // ... add more children
+        return $menu;
+    }
+
+    public function createUserMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+        
+        $user = $this->security->getUser();
+        $child = $menu->addChild($user->getEmail(), ['uri' => '#']);
+        $child->addChild('Se déconnecter', ['route' => 'app_logout']);
+
         return $menu;
     }
 }
